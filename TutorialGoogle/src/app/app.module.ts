880bf867844dbc7440ba98861e041aea7a9d9f73
Israel Reyes <potcheunam@gmail.com>
2016-12-14 16:58:01 -0600

    http

--- TutorialGoogle/src/app/app.module.ts
+++ TutorialGoogle/src/app/app.module.ts
@@ -1,27 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
+import './rxjs-extensions';
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroService } from './hero-detail/hero.service';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero-detail/hero.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
---
+++ TutorialGoogle/src/app/hero-search/hero-search.service.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by isra on 14/12/16.
+ */
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+
+import { Hero } from '../hero-detail/hero';
+
+@Injectable()
+export class HeroSearchService {
+
+  constructor(private http: Http) {}
+
+  search(term: string): Observable<Hero[]> {
+    return this.http
+      .get(`app/heroes/?name=${term}`)
+      .map((r: Response) => r.json().data as Hero[]);
+  }
+}
